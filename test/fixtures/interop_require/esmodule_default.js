522b8d059e9e54a8c6867371e6287ee4a023bf91
AngrySean <xujihui1985@gmail.com>
2017-05-16 10:01:10 +0800

    fix: remove interop-require from dependency (#33)

---
+++ test/fixtures/interop_require/esmodule_default.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class Hello {
+    constructor() {
+    }
+    world() {
+      return 'hello world';
+    }
+}
+exports.default = Hello;
